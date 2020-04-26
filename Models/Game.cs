52be05d81d15ed430d569eb314c0b3465f876dc1
Pranav Krishnamurthy <v-prkris@microsoft.com>
2020-04-26 14:37:37 -0700

    Adding the necessary models for the game

---
+++ Models/Game.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="Game.cs" company="Tata Consultancy Services Ltd">
+// Copyright (c) Tata Consultancy Services Ltd. All rights reserved.
+// </copyright>
+
+namespace BotDontLie.Models
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This class models a single game.
+    /// </summary>
+    public class Game
+    {
+    }
+}
