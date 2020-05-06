d8f59b77a3ca6b90125f89092c609ca0141dbeee
Pranav Krishnamurthy <v-prkris@microsoft.com>
2020-05-06 16:53:20 -0700

    Updating the comments

--- Services/BallDontLieService.cs
+++ Services/BallDontLieService.cs
@@ -6,18 +6,35 @@ namespace BotDontLie.Services
 {
     using System.Threading.Tasks;
     using BotDontLie.Models;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
     /// <summary>
     /// This class will implement the methods that are defined in the interface <see cref="IBallDontLieService"/>.
     /// </summary>
     public class BallDontLieService : IBallDontLieService
     {
+        private readonly TelemetryClient telemetryClient;
+        private readonly IHttpClientFactory httpClientFactory;
+
         /// <summary>
-        /// Method implementation to return all 30 of the NBA franchises.
+        /// Initializes a new instance of the <see cref="BallDontLieService"/> class.
+        /// </summary>
+        /// <param name="telemetryClient">Application Insights DI.</param>
+        /// <param name="httpClientFactory">The HTTP Client Factory DI.</param>
+        public BallDontLieService(TelemetryClient telemetryClient, IHttpClientFactory httpClientFactory)
+        {
+            this.telemetryClient = telemetryClient;
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        /// <summary>
+        /// Method implementation to return all 30 NBA franchises.
         /// </summary>
         /// <returns>A unit of execution that contains the type of <see cref="TeamsResponse"/>.</returns>
         public async Task<TeamsResponse> RetrieveAllTeams()
         {
+            this.telemetryClient.TrackTrace("Requesting to get all NBA teams");
             return null;
         }
     }
