d18285fd3705cde40c25ed588074677194ee6f01
Pranav Krishnamurthy (TEAMS) <Pranav.Teams@succeedwithtcs.com>
2020-05-13 13:14:20 -0700

    Adding changes

---
+++ Providers/ITeamsProvider.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="ITeamsProvider.cs" company="Tata Consultancy Services Ltd">
+// Copyright (c) Tata Consultancy Services Ltd. All rights reserved.
+// </copyright>
+
+namespace BotDontLie.Providers
+{
+    /// <summary>
+    /// This interface defines the methods to insert data into Azure table storage.
+    /// </summary>
+    public interface ITeamsProvider
+    {
+    }
+}
---
+++ Providers/TeamsProvider.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="TeamsProvider.cs" company="Tata Consultancy Services Ltd">
+// Copyright (c) Tata Consultancy Services Ltd. All rights reserved.
+// </copyright>
+
+namespace BotDontLie.Providers
+{
+    /// <summary>
+    /// This class implements methods defined in <see cref="ITeamsProvider"/>.
+    /// </summary>
+    public class TeamsProvider : ITeamsProvider
+    {
+    }
+}
