4d02591c576982c9ee6f8f25e180cbbbedefe381
Pranav Krishnamurthy <v-prkris@microsoft.com>
2020-05-13 22:39:56 -0700

    Having necessary things here for making sure to bring in data into the Azure table storage

---
+++ Models/AzureStorage/TeamEntity.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="TeamEntity.cs" company="Tata Consultancy Services Ltd">
+// Copyright (c) Tata Consultancy Services Ltd. All rights reserved.
+// </copyright>
+
+namespace BotDontLie.Models
+{
+    using Microsoft.WindowsAzure.Storage.Table;
+    using System;
+
+    /// <summary>
+    /// This class defines the NBA Team to be stored in Azure table storage.
+    /// </summary>
+    public class TeamEntity : TableEntity
+    {
+    }
+}
--- Providers/TeamsProvider.cs
+++ Providers/TeamsProvider.cs
@@ -4,10 +4,26 @@
 
 namespace BotDontLie.Providers
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.WindowsAzure.Storage.Table;
+
     /// <summary>
     /// This class implements methods defined in <see cref="ITeamsProvider"/>.
     /// </summary>
     public class TeamsProvider : ITeamsProvider
     {
+        private const string PartitionKey = "NbaTeam";
+        private readonly Lazy<Task> initializeTask;
+        private CloudTable teamCloudTable;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TeamsProvider"/> class.
+        /// </summary>
+        /// <param name="connectionString">The Azure Table connection string.</param>
+        public TeamsProvider(string connectionString)
+        {
+            this.initializeTask = new Lazy<Task>(() => this.InitializeTableStorageAsync(connectionString));
+        }
     }
 }
